f4030c3281fb9b795381cd653a477bd4af6b5925
Estorko <anthony.ibrahim.deo@gmail.com>
2021-12-07 22:18:00 +0200

    older version (need update )

--- Sql1.sql
+++ Sql1.sql
@@ -1,2 +1,21 @@
 ﻿create database T23_10849;
-create table 
+create table Student
+(
+id int Primary Key Identity,
+first_name varchar(20), 
+last_name varchar(20), 
+email varchar(50), 
+password varchar(20), 
+type bit,
+faculty varchar(20),
+address varchar(10),
+GPA decimal (3,2)
+)
+create table GucianStudent
+(id int primary key references Student(id),undergradID int not null)
+create table NonGucianStudent
+(id int primary key references Student(id),underGradId int not null)
+create table GUCStudentPhoneNumber
+(id int primary key references GucianStudent(id),
+phone
+
